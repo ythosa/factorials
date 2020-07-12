2cb323a57cb6c8d0c47863524af54c76e65ad6d4
Ythosa <vasus714@yandex.ru>
2020-07-12 22:16:57 +0300

    Refactoring using vector

--- Factorials/Factorials.cpp
+++ Factorials/Factorials.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <vector>
 
 #define ull unsigned long long
 
 ull const simple_values[] = { 1,1,1,3 };
 
-template <typename type>
-type isqrt(type remainder)
+template <typename T>
+T isqrt(T remainder)
 {
     if (remainder < 0) // if type is unsigned this will be ignored = no runtime  
         return 0; // negative number ERROR  
 
-    type place = (type)1 << (sizeof(type) * 8 - 2); // calculated by precompiler = same runtime as: place = 0x40000000  
+    type place = (T)1 << (sizeof(T) * 8 - 2); // calculated by precompiler = same runtime as: place = 0x40000000  
     while (place > remainder)
         place /= 4; // optimized by complier as place >>= 2  
 
-    type root = 0;
+    T root = 0;
     while (place)
     {
         if (remainder >= root + place)
@@ -34,13 +35,13 @@ type isqrt(type remainder)
     insert just before the leftmost x already there.
     Optional args lo (default 0) and hi (default len(a)) bound the
     slice of a to be searched. */
-ull bisect_left(ull a[], ull x, ull lo = 0, ull hi = -1) {
+ull bisect_left(std::vector<ull> a, ull x, ull lo = 0, ull hi = -1) {
     if (lo < 0) {
         throw "lo must be non-negative";
     }
 
     if (hi == -1) {
-        hi = sizeof(a) / sizeof(*a);
+        hi = a.size();
     }
 
     ull mid;
@@ -57,7 +58,7 @@ ull bisect_left(ull a[], ull x, ull lo = 0, ull hi = -1) {
     return lo;
 }
 
-ull product(ull s[], ull n, ull m) {
+ull product(std::vector<ull> s, ull n, ull m) {
     if (n > m) {
         return 1;
     }
@@ -69,6 +70,19 @@ ull product(ull s[], ull n, ull m) {
     ull k = (n + m) / 2;
 
     return product(s, n, k) * product(s, k + 1, m);
+}
+
+ull swing(ull m, std::vector<ull> primes) {
+
+    if (m < 4) {
+        return simple_values[m];
+    };
+
+    ull s = bisect_left(primes, 1 + isqrt(m));
+    ull d = bisect_left(primes, 1 + m / 3);
+    ull e = bisect_left(primes, 1 + m / 2);
+    ull g = bisect_left(primes, 1 + m);
+
 }
 
 int main() {
